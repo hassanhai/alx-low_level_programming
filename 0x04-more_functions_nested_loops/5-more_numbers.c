650a14dbc3d5c9871060a631bc0994c95d8565ef
hassanhai <hassan.mouilha@gmail.com>
2023-03-24 18:25:03 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int n, m;
+	
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m > 9)
+			{
+				_putchar((m / 10) + '0');
+			}
+			_putchar ((m % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
