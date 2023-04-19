87fcb0b4e36fbd780826535c1fcf5e775479f182
hassanhai <hassan.mouilha@gmail.com>
2023-04-19 16:42:58 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,14 +6,17 @@
  * @cmp: pointer to func of one of the 3 in main
  * Return: 0
  */
-int int_index(int *array, int size, int (*cmp)(int)) {
-    if (size <= 0) {
-        return -1;
-    }
-    for (int i = 0; i < size; i++) {
-        if (cmp(array[i]) != 0) {
-            return i;
-        }
-    }
-    return -1;
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
